5f5a43f57cb06cdc763a05c04b2401c57abc1737
Mankz111 <mgacp199@gmail.com>
2025-11-20 15:11:45 +0000

    Updated Histograms for better visual

--- FIA/Linear_Regression.py
+++ FIA/Linear_Regression.py
@@ -9,28 +9,29 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 csv = os.path.join(os.path.dirname(__file__), 'portugal_real_estate.csv')
-
 data = pd.read_csv(csv, low_memory=False)
 
 #print(data['TotalArea'].describe())
 
 clean_area = (data['TotalArea'] > 10) & (data['TotalArea']<5000)
 area_df_clean = data[clean_area]
-plt.hist(area_df_clean['TotalArea'], bins=10, edgecolor = 'black')
-plt.xlabel("Total Area")
-plt.ylabel("Frequency")
-plt.title("Total Area of real estate in Portugal")
-
 filter = (data['Price'] < 1500000) & (data['Price'] > 10000)
 df_clean = data[filter]
-plt.hist(df_clean['Price'] / 1000, bins=50, edgecolor = 'black')
-plt.ticklabel_format(style='plain', axis='x')
-plt.title('Price Houses in Portugal')
-plt.xlabel('Preço em milhares de €')
-plt.ylabel('Frequência')
-ax = plt.gca()
-ax.format_coord = lambda x, y: f"x={x:.0f} y={y:.0f}"
+
+fig, (ax1,ax2) = plt.subplots(1,2, figsize=(12,5))
+#Gráfico 1
+ax1.hist(area_df_clean['TotalArea'], bins=10, edgecolor='black')
+ax1.set_xlabel("Total Area")
+ax1.set_ylabel("Frequency")
+ax1.set_title("Total Area of real estate in Portugal")
+#Gráfico 2
+
+ax2.hist(df_clean['Price'], bins=10, edgecolor='black')
+ax2.set_xlabel("Price")
+ax2.set_ylabel("Frequency")
+ax2.set_title("Price of Houses in Portugal")
 plt.show()
+
 
 #print(data.info())
 
